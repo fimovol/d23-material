4200049ab4c8504c574433a010e7dc8cf9c80960
fimovol <fimovolpe@gmail.com>
2023-01-30 12:20:47 -0500

    test: import HttpClientTestingM in service test

--- src/app/services/userservice.service.spec.ts
+++ src/app/services/userservice.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { UserserviceService } from './userservice.service';
 
 describe('UserserviceService', () => {
   let service: UserserviceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(UserserviceService);
   });
 
